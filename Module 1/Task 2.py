b95988040819c19cd18cacc7e2f603a44af828b2
rabinpanthisnr <rabinpanthi2920@gmail.com>
2025-09-04 12:41:44 +0545

    I have done th module 1

---
+++ Module 1/Task 2.py
@@ -0,0 +1,3 @@
+radius = int(input("Enter the radius of the circle: "))
+area = 3.14 * radius * radius
+print("The radius of the circle is: ", area)
---
+++ Module 1/Task6.py
@@ -0,0 +1,12 @@
+import random
+random_1=random.randint(0,9)
+random_2=random.randint(0,9)
+random_3=random.randint(0,9)
+print(random_1,random_2,random_3)
+
+
+random_1=random.randint(0,6)
+random_2=random.randint(0,6)
+random_3=random.randint(0,6)
+random_4=random.randint(0,6)
+print(f"{random_1}{random_2}{random_3}{random_4}")
